455ad5eee1a92ff1ca9bcb609c0fc5afc8b77275
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-21 16:23:47 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker[] workers = new Worker[5];
+            for (int i = 0; i < workers.Length;)
+            {
+                Console.WriteLine("Введите данные о работнике в формате [ФИО ; Должность ; Год принятия на работу]");
+                string[] inputString = Console.ReadLine().Split(';');
+                try
+                {
+                    workers[i] = new Worker(inputString[0], inputString[1], inputString[2]);
+                    i++;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Используйте следующий формат. Ошибка: " + e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Array.Sort(workers);
+            Console.WriteLine("Укажите необходимы стаж работника.");
+            int filter;
+            while (true)
+            {
+                try
+                {
+                    filter = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            Console.WriteLine($"Данные работники работали больше чем {filter} (лет)");
+            foreach (var item in workers)
+            {
+                if (item.IsWorkedLessThen(filter))
+                {
+                    Console.Write(item.GetInfo());
+                }
+            }
+        }
+    }
+}
---
+++ Task2/Worker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task2
+{
+    internal struct Worker : IComparable<Worker>
+    {
+        private string name;
+        private string position;
+
+        private DateTime yearOfEmpl;
+
+        public Worker(string name, string position, string yearOfEmpl)
+        {
+            this.name = name;
+            this.position = position;
+            this.yearOfEmpl = DateTime.Parse(yearOfEmpl);
+        }
+
+        public int CompareTo(Worker other)
+        {
+            return name.CompareTo(other.name);
+        }
+        public string GetInfo()
+        {
+            return $"ФИО: {name} \nДолжность: {position}\nГод принятия на работу: {yearOfEmpl.Year}\n";
+        }
+        public bool IsWorkedLessThen(int year)
+        {
+            return year < DateTime.Now.Year - yearOfEmpl.Year;
+        }
+    }
+}
